bfa196696e901bdaf3b1ef23c66815f4253cc805
Sarah Quinney <quinney.sarah@gmail.com>
2023-06-21 17:33:01 -0500

    updated comments

--- src/index.js
+++ src/index.js
@@ -1,26 +1,9 @@
 import $ from "jquery";
 
-// $(document).ready(function () {
-// $('.dashboard-text').on("click",
-//     function () {
-//         //show menu
-//         $('.dashboard-list', this).hide();
-//     }, function () {
-//         //hide menu
-//         $('.dashboard-list', this).show();
-//     });
-// // });
+
 $(document).ready(function () {
     $('.dashboard-text').on("click", () => {
         $('.dashboard-list').toggle();
     })
 }) 
 
-
-// $('.dashboard-text').on('click', function() {
-//     $('.dashboard-list').toggleClass('hide)')
-// })
-
-// $(".dashboard-text").on("click", function(){
-//     $(this).toggleClass("active");
-//   });
